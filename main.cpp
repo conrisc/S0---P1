91d7fc141453e32079f4647f8fbe764e14f9cd52
Konrad Cielecki <conrisc@gmail.com>
2016-11-21 17:38:17 +0100

    just update with master

--- main.cpp
+++ main.cpp
@@ -10,22 +10,20 @@
 
 using namespace std;
 
-sem_t gnomes, gnomeSem[3], toyMut;
-int blockedGnome;
-int createdToy;
+sem_t gnomes, gnomeSem[3],santaSem, toyMut;
+int blockedGnome, createdToy, toy[3];
 bool firstPartNotCreated=true;
-int toy[3];
 
 void *gnome(void *gnomeID) {
   int gID = (long)gnomeID;
   while (true) {
-    sleep(1);
+    // sleep(1);
     sem_wait(&gnomes);
-    cout<<gID<<":\n";
+    // cout<<gID<<":\n";
     if(firstPartNotCreated) {
       createdToy=gID;
       firstPartNotCreated=false;
-      cout<<"  "<<gID<<" Stworzylem pierwsza czesc\n";
+      cout<<gID<<" Stworzylem pierwsza czesc\n";
       blockedGnome=gID;
       sem_post(&gnomes);
       sem_wait(&gnomeSem[gID]);
@@ -34,27 +32,48 @@ void *gnome(void *gnomeID) {
       createdToy+=gID;
       sem_wait(&toyMut);
       toy[createdToy-1]++;
+      cout<<gID<<" Dokonczylem tworzyc zabawke\n";
       cout<<  "BUFFOR "<<createdToy<<": "<<toy[createdToy-1]<<endl;
       sem_post(&toyMut);
       firstPartNotCreated=true;
-      cout<<"  "<<gID<<" Dokonczylem tworzyc zabawke\n";
       sem_post(&gnomeSem[blockedGnome]);
       sem_post(&gnomes);
     }
   }
 }
 
+void *santa(void *sth) {
+  while (true) {
+    sem_wait(&santaSem);
+
+    sem_wait(&toyMut);
+    cout << "\rsanta(): Zabawki się uzbierały, wstaję do pracy..." << endl << flush;
+
+    for (int i = 0; i < 3; i++)
+      toy[i] -= 3;
+
+    cout << "\rsanta(): I po robocie, zabawki wysłane. Czas iść spać..." << flush;
+    sem_post(&toyMut);
+  }
+  pthread_exit(NULL);
+}
+
 int main() {
   sem_init(&toyMut,0,1);  //mutex na buffory (wszystkie 3)
   sem_init(&gnomes,0,1);
+  sem_init(&santaSem, 0, 0);
   sem_init(&gnomeSem[0],0,0);
   sem_init(&gnomeSem[1],0,0);
   sem_init(&gnomeSem[2],0,0);
 
   pthread_t threads[3];
 
+  pthread_create(&threads[3], NULL, santa, NULL);
+
   for (long i=0;i<3;i++)
     pthread_create(&threads[i], NULL, gnome, (void *)i);
+
+
 
   for (int i=0;i<3;i++)
     pthread_join(threads[i], NULL);
